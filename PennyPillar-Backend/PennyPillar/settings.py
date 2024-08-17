df400eb6326bcca815b26d053e43eb93d602c262
Tau <taumasaire@gmail.com>
2024-08-17 21:27:54 +0200

    Updated javascript logic for sending requests to the backend for the Penny Challenge

--- PennyPillar-Backend/PennyPillar/settings.py
+++ PennyPillar-Backend/PennyPillar/settings.py
@@ -66,6 +66,9 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 MIDDLEWARE = [
