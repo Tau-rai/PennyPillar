517b15350b20614a9cba2496648f766c4a3b37c0
Aevy21 <137657540+Aevy21@users.noreply.github.com>
2024-08-21 01:22:56 +0200

    Update Challenge.js

--- PennyPillar-Frontend/src/components/Challenge/Challenge.js
+++ PennyPillar-Frontend/src/components/Challenge/Challenge.js
@@ -1,4 +1,4 @@
-.import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Challenge.css';
 import axiosInstance from '../../axiosConfig';
 import MainFooter from '../ComponentFooter';
