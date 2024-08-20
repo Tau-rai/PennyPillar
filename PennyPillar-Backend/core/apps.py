4731a2a3eb1381cbc12771887a29dff8b59e88e4
Taurai <68677265+Tau-rai@users.noreply.github.com>
2024-08-20 10:31:27 +0200

    Update apps.py with a Job for daily insights

--- PennyPillar-Backend/core/apps.py
+++ PennyPillar-Backend/core/apps.py
@@ -1,6 +1,16 @@
+"""Module for apps."""
 from django.apps import AppConfig
-
+from django_appscheduler.models import Job
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # Register or verify job schedule
+        Job.objects.get_or_create(
+            name="Generate Daily Insight",
+            task="core.cron.generate_daily_insight_task",
+            schedule="daily",
+            defaults={"enabled": True}
+        )
