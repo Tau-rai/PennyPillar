47944ac67fc3d26edb450cbca47d20f26e671412
Taurai <68677265+Tau-rai@users.noreply.github.com>
2024-08-25 22:04:51 +0200

    Update settings.py

--- PennyPillar-Backend/PennyPillar/settings.py
+++ PennyPillar-Backend/PennyPillar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', secrets.token_hex(32))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = [ 'pennypillarapi.onrender.com', 'localhost', '127.0.0.1' ]
 
 
 # Application definition
